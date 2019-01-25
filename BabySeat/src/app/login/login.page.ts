ae70bdee4bdc607a1ae5f337337f344d7d09cc61
Giovanni Leo <giovannileo595@gmail.com>
2019-01-25 18:32:34 +0100

    Fix for web

--- BabySeat/src/app/login/login.page.ts
+++ BabySeat/src/app/login/login.page.ts
@@ -13,13 +13,16 @@ import {Diagnostic} from '@ionic-native/diagnostic/ngx';
 })
 export class LoginPage implements OnInit {
 
-  webFlag = false;
+  webFlag = true;
 
   constructor(private geolocation: Geolocation, private router: Router, private toastController: ToastController,
               private backGround: BackgroundMode, private platform: Platform,
               private diagnostic: Diagnostic) { }
 
   ngOnInit() {
+    if (this.webFlag) {
+      this.getPositionOnWeb(false);
+    }
     this.platform.ready().then((rdy) => {
 
       this.diagnostic.getLocationMode().then((state) => {
@@ -45,6 +48,11 @@ export class LoginPage implements OnInit {
   }
 
   login() {
+    if (this.webFlag) {
+       this.getPositionOnWeb(true);
+    }
+
+
     this.diagnostic.getLocationMode().then((state) => {
       if (state === this.diagnostic.locationMode.LOCATION_OFF) {
         this.presentToastWithOptions('Attiva la geolocalizzazione');
@@ -77,6 +85,21 @@ export class LoginPage implements OnInit {
     toast.present();
   }
 
+
+  getPositionOnWeb(navigate: boolean) {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      // resp.coords.latitude
+      // resp.coords.longitude
+      console.log(resp.coords.latitude + ' ' + resp.coords.longitude);
+      console.log('else loc');
+      if (navigate === true) {
+        this.router.navigate(['/home']);
+      }
+    }).catch((error) => {
+      console.log('Error getting location', error);
+      this.presentToastWithOptions('Errore nell\' ottenimento della poaizione, perfavore riprova');
+    });
+  }
 
 
 }
